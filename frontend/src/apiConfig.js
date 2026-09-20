9e75a5ef1b98a4b7b798b2c546e98e08f955c431
dev-raaaj <btechshayar@gmail.com>
2026-09-20 19:38:07 +0530

    chore: link live render backend in apiConfig

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -3,4 +3,4 @@
 export const API_BASE = 
   window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? 'http://127.0.0.1:8000'
-    : '';
+    : 'https://localops.onrender.com';
